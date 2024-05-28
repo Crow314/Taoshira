1a86c36be95b431d683f96d6cf41689b7e0625e5
Crow314 <me@crow31415.net>
2024-05-28 12:42:35 +0900

    Add acceleration view for MainActivity

--- app/src/main/java/net/crow31415/taoshira/MainActivity.kt
+++ app/src/main/java/net/crow31415/taoshira/MainActivity.kt
@@ -2,6 +2,10 @@ package net.crow31415.taoshira
 
 import android.app.ActivityManager
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.LocationManager
 import android.os.Bundle
 import android.widget.Button
@@ -10,10 +14,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var mAccelerationTextView: TextView
 
+    private lateinit var mSensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +33,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        mAccelerationTextView = findViewById(R.id.accelerationTextView)
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val accelerationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        mSensorManager.registerListener(this, accelerationSensor, SensorManager.SENSOR_DELAY_UI)
+
         val startButton: Button = findViewById(R.id.startButton)
         startButton.setOnClickListener {
             val serviceIntent = Intent(this, ShockDetectService::class.java)
             startForegroundService(serviceIntent)
         }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) {
+            return
+        }
+
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            //ベクトル合成
+            val acceleration = sqrt(x.pow(2) + y.pow(2) + z.pow(2))
+
+            mAccelerationTextView.text = acceleration.toString()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Do nothing
     }
 }
